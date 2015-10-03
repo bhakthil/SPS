98dfe8e06557eeba2522af59effd99a6d5a150e9
bhakthil <bhakthil@gmail.com>
2015-10-02 22:05:56 -0400

    Reston

---
+++ Reston/Get-List.ps1
@@ -0,0 +1,27 @@
+﻿Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+$siteUrl = “https://liyanage.sharepoint.com/sites/developer” ;
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl) ;
+
+<# Authenticate with the SharePoint Online site. #>
+if ($global:spoCred -eq $null) { 
+    $password = Read-Host -Prompt "Enter password" -AsSecureString;
+    #$cred = Get-Credential -Message "Enter your credentials for SharePoint Online:" ;
+    $spoCred = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials("bhakthil@liyanage.onmicrosoft.com", $password) ;
+} 
+
+$ctx.Credentials = $spoCred;
+
+$list = $ctx.Web.Lists.getByTitle('Customers');
+$ctx.Load($list);
+$ctx.ExecuteQuery() ;
+
+$id = $list.Id;
+
+$count = $list.ItemCount;
+
+
+Write-Host $list.Title;
+Write-Host $id;
+Write-Host $count;
---
+++ Reston/Set-ListItem.ps1
@@ -0,0 +1,30 @@
+﻿Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll" 
+Add-Type –Path "C:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll" 
+
+$siteUrl = “https://liyanage.sharepoint.com/sites/developer” ;
+$ctx = New-Object Microsoft.SharePoint.Client.ClientContext($siteUrl) ;
+
+<# Authenticate with the SharePoint Online site. #>
+if ($global:spoCred -eq $null) { 
+    $password = Read-Host -Prompt "Enter password" -AsSecureString;
+    #$cred = Get-Credential -Message "Enter your credentials for SharePoint Online:" ;
+    $spoCred = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials("bhakthil@liyanage.onmicrosoft.com", $password) ;
+} 
+
+$ctx.Credentials = $spoCred;
+
+$list = $ctx.Web.Lists.getByTitle('Announcements');
+
+
+
+$listItem = $list.GetItemById(6);
+
+$listItem["Title"] = "SPSCT15";
+$listItem.Update();
+
+$ctx.ExecuteQuery() ;
+
+
+
+
+
